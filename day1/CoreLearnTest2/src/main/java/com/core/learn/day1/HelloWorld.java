c89dae9946b65dbea9f86d48681ca5e8166a3329
ganesh <rk.kravanth@gmail.com>
2018-10-06 23:12:05 +0530

    day1testHelloworld

---
+++ day1/CoreLearnTest2/src/main/java/com/core/learn/day1/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.core.learn.day1;
+
+/**
+ * @author ganesh
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is my first git maven Hello World program");
+
+	}
+
+}
